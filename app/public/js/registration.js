9b26257ef112ca62d68730661c3f577172a0dfdf
Bibin Tom <bibintomj@gmail.com>
2018-08-08 12:35:09 +0530

    Added SQL DB Fetch

--- app/public/js/registration.js
+++ app/public/js/registration.js
@@ -1,6 +1,6 @@
 $(function() {
   $('.registration-form').submit(function (e) {
     e.preventDefault()
-
+    //add to DB
   })
 })
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,41 @@
-let express = require('express')
-let router = express.Router()
+const express = require('express')
+const mysql = require('mysql')
+const router = express.Router()
 
-router.get('/', function (req, res) {
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Pass@123',
+  database: 'AppleDatabase'
+})
+
+router.get('/', (req, res) => {
   res.render('index', {})
+})
+
+router.get('/user/:id', (req, res) => {
+  const query = 'SELECT * from users where id = ?'
+  connection.query(query, [req.params.id], (err, rows, fields) => {
+    if (err) {
+      console.log('Failed to fetch' + err)
+      res.sendStatus(500)
+      return
+    }
+    res.json(rows)
+  })
+})
+
+router.get('/users', (req, res) => {
+  const query = 'Select * from users'
+
+  connection.query(query, (err, rows, fields) => {
+    if (err) {
+      console.log('Failed to fetch' + err)
+      res.sendStatus(500)
+      return
+    }
+    res.render('users', {'users': rows})
+  })
 })
 
 module.exports = router
